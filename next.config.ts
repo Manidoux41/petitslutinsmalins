697ab45241c309199626467c2c06280d652bca4f
Manfred Parbatia <lherissondu41@gmail.com>
2025-04-23 20:59:27 +0200

    modif

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true, // Active les Server Actions
+  },
+  images: {
+    domains: ["cdn.shopify.com", "images.unsplash.com"], // adapte si tu utilises d'autres domaines
+  },
+  reactStrictMode: true,
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
